145ceec4001fb59fea05be1e62b6d65bfa557445
erickleite338 <erickleite338@gmail.com>
2024-08-20 13:37:11 +0000

    chore(dev): formatar código com idx-nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{pkgs}: {
+{ pkgs }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
@@ -11,7 +11,7 @@
     pkgs.bun
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -27,10 +27,14 @@
         # Open editors for the following files by default, if they exist:
         default.openFiles = [
           # Cover all the variations of language, src-dir, router (app/pages)
-          "pages/index.tsx" "pages/index.jsx"
-          "src/pages/index.tsx" "src/pages/index.jsx"
-          "app/page.tsx" "app/page.jsx"
-          "src/app/page.tsx" "src/app/page.jsx"
+          "pages/index.tsx"
+          "pages/index.jsx"
+          "src/pages/index.tsx"
+          "src/pages/index.jsx"
+          "app/page.tsx"
+          "app/page.jsx"
+          "src/app/page.tsx"
+          "src/app/page.jsx"
         ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
@@ -40,7 +44,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
